a8d42847936fcafb97aaa7ad142931ea29ca96d1
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2023-01-26 22:51:43 -0300

    OO_TCA

---
+++ src/main/java/ifpr/pgua/eic/projetointegrador/model/entities/Setor.java
@@ -0,0 +1,30 @@
+package ifpr.pgua.eic.projetointegrador.model.entities;
+
+import javafx.scene.control.DatePicker;
+
+public class Setor {
+    private String codigo;
+    private String descricao;
+
+    public Setor(String codigo, String descricao){
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setDescricao(){
+        this.descricao = descricao;
+    }
+    
+}
