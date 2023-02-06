459b25cde3cef1c339ad2ed361574f039ba3cfbc
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2023-02-05 23:49:12 -0300

    OO_TCA

--- src/main/java/ifpr/pgua/eic/projetointegrador/controllers/TelaComputador.java
+++ src/main/java/ifpr/pgua/eic/projetointegrador/controllers/TelaComputador.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 
 import ifpr.pgua.eic.projetointegrador.model.entities.Computador;
 import ifpr.pgua.eic.projetointegrador.model.repositories.ComputadorRepositorio;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
@@ -15,34 +16,47 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class TelaComputador extends BaseController implements Initializable{
 
-    private Button btCadastar;
+    @FXML
+    private Button btCadastrar;
 
+    @FXML
     private Button btEditar;
 
+    @FXML
     private Button btExcluir;
 
+    @FXML
     private TextField tfNome;
 
+    @FXML
     private TextField tfIp;
 
+    @FXML
     private TextField tfPatrimonio;
 
+    @FXML
     private DatePicker dpDataMT;
 
+    @FXML
     private TableView<Computador> tbComputador;
 
+    @FXML
     private TableColumn<Computador, String> tbcIp;
 
+    @FXML
     private TableColumn<Computador, String> tbcPatrimonio;
 
+    @FXML
     private TableColumn<Computador, String> tbcNome;
 
-
+    @FXML
     private ComputadorRepositorio repositorio;
 
     public TelaComputador(ComputadorRepositorio repositorio){
         this.repositorio = repositorio;
     }
+
+
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
@@ -54,17 +68,20 @@ public class TelaComputador extends BaseController implements Initializable{
         
     }
 
+    @FXML
     private void atualizarTabela() {
         tbComputador.getItems().clear();
         tbComputador.getItems().addAll(repositorio.mostrarTodos());
     }
 
+    @FXML
     private void adicionar(){
         String nome = tfNome.getText();
         String Ip = tfIp.getText();
         String Patrimonio = tfPatrimonio.getText();
     }
 
+    @FXML
     private void Limpar(){
         tfNome.clear();
         tfIp.clear();
